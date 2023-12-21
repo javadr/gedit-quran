81ee1e43ecca05180f4e00e8a6d47799374de6a7
javad <javadr@gmail.com>
2023-12-22 01:54:40 +0330

    (style) type hint

--- src/quran/quran.py
+++ src/quran/quran.py
@@ -1,7 +1,7 @@
 import gzip
 
 from pathlib import Path
-from typing import Tuple, List, Union
+from typing import Tuple, List, Union, Optional
 
 # region ############### CONSTANTs #################################
 SOURCE_DIR = Path(__file__).parent.resolve()
@@ -25,7 +25,7 @@ class Quran:
         self.suras_ayat = tuple(map(int, st.strip().split(",")))
         self.line_index = self._create_index()
 
-    def _create_index(self):
+    def _create_index(self) -> Tuple[int]:
         index = []
         position = 0
         with gzip.open(self.quran_file, "rt") as gzipped_file:
@@ -35,8 +35,11 @@ class Quran:
                 position = gzipped_file.tell()
         return tuple(index)
 
-    def get_verse(self, sura, from_ayah, to_ayah=None):
-        Ayat: List(str, int) = []
+    def get_verse(
+            self, sura: int,
+            from_ayah: int, to_ayah: Optional[int] = None,
+            ) -> List[Tuple[str, int]]:
+        Ayat: List[Tuple[str, int]] = []
         if to_ayah is None:
             to_ayah = from_ayah
         with gzip.open(self.quran_file, "rt") as gzipped_file:
